2ff47ee6d2c1d016f7ec30c8a054869258e162e0
sunDalik <dali1999@mail.ru>
2018-11-05 20:58:44 +0300

    well... that's something I guess...

--- LAB 4/main.c
+++ LAB 4/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include <assert.h>
 #include "linked_list.h"
 #include "file_io.h"
 #include "list_higher_order.h"
@@ -24,16 +25,22 @@ int main() {
 	int choice;
 	char filename[64];
 
-	puts("Enter 1 to read from file or 2 to read from stdin");
+	puts("Enter 1 to read from file, 2 to load from binary or 3 to read from stdin");
     scanf("%d", &choice);
     switch (choice){
         case 1: {
         	puts("Enter file name:");
         	scanf("%63s", filename);
-            load(&list, "filename");
+            load(&list, filename);
             break;
         }
         case 2: {
+            puts("Enter file name:");
+            scanf("%63s", filename);
+            deserialize(&list, filename);
+            break;
+        }
+        case 3: {
         	puts("Enter list elements delimited by whitespace and finish the input with ^D");
     		while (scanf("%d", &e) != EOF) { 
         		list_add_front(&list, e);
@@ -41,15 +48,16 @@ int main() {
             break;
         }
         default: {
-            printf("Unknown command\n");
+            puts("Unknown command");
             return 0;
         }
     }
 
     
     puts("");
-    list_info(list);
-    puts("\nList elements in a row:");
+    printf("List length: %d\n", list_length(list));
+    printf("Sum: %d\n", foldl(list, 0, sum));
+    puts("List elements in a row:");
     foreach(list, print_space);
     puts("\nList elements in a collumn:");
     foreach(list, print_newline);
@@ -57,19 +65,37 @@ int main() {
     printf("%d\n", foldl(list, INT_MAX, min));
     puts("Max element:");
     printf("%d\n", foldl(list, INT_MIN, max));
-    puts("Sum:");
-    printf("%d\n", foldl(list, 0, sum));
     puts("Squares:");
     foreach(map(list, square), print_space);
     puts("\nCubes:");
     foreach(map(list, cube), print_space);
-    puts("\nModules:");
+    puts("\nAll values were changed to absolute");
     map_mut(list, abs);
     foreach(list, print_space);
     puts("\nFirst 10 powers of 2:");
     foreach(iterate(10, 1, mul_by2), print_space);
-    puts("");
 
+    puts("\nEnter 1 to save to file, 2 to save to binary or anything else to just exit.");
+    scanf("%d", &choice);
+    switch (choice){
+        case 1: {
+            puts("Enter file name:");
+            scanf("%63s", filename);
+            save(list, filename);
+            break;
+        }
+        case 2: {
+            puts("Enter file name:");
+            scanf("%63s", filename);
+            serialize(list, filename);
+            break;
+        }
+        default: {
+            puts("We appreciate your choice");
+        }
+    }
 
+    list_free(&list);
+    assert(list == NULL);
     return 0;
 }
