81df5cdfd178e345e5fcce58119654c858c926db
sunDalik <dali1999@mail.ru>
2018-12-04 11:14:09 +0300

    polished

--- LAB5/bmp_struct.h
+++ LAB5/bmp_struct.h
@@ -1,35 +1,34 @@
 #include <stdint.h>
+
 #ifndef LAB5_BMP_STRUCT_H
 #define LAB5_BMP_STRUCT_H
 
- struct __attribute__((packed)) bmp_header  {
+struct __attribute__((packed)) bmp_header {
+    uint16_t bfType;
+    uint32_t bfileSize;
+    uint32_t bfReserved;
+    uint32_t bOffBits;
 
-     uint16_t bfType;
-     uint32_t bfileSize;
-     uint32_t bfReserved;
-     uint32_t bOffBits;
-
-     uint32_t biSize;
-     uint32_t biWidth;
-     uint32_t biHeight;
-     uint16_t biPlanes;
-     uint16_t biBitCount;
-     uint32_t biCompression;
-     uint32_t biSizeImage;
-     uint32_t biXPelsPerMeter;
-     uint32_t biYPelsPerMeter;
-     uint32_t biClrUsed;
-     uint32_t biClrImportant;
-
+    uint32_t biSize;
+    uint32_t biWidth;
+    uint32_t biHeight;
+    uint16_t biPlanes;
+    uint16_t biBitCount;
+    uint32_t biCompression;
+    uint32_t biSizeImage;
+    uint32_t biXPelsPerMeter;
+    uint32_t biYPelsPerMeter;
+    uint32_t biClrUsed;
+    uint32_t biClrImportant;
 };
 
 struct pixel {
-    unsigned char r,g,b;
+    unsigned char r, g, b;
 };
 
 struct image {
     uint32_t width, height;
-    struct pixel* data;
+    struct pixel *data;
 };
 
 #endif
