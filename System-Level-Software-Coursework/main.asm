a174ece727b422036605702f3317d7056d000836
sunDalik <dali1999@mail.ru>
2019-06-09 12:26:09 +0300

    let's think it through...

---
+++ System-Level-Software-Coursework/main.asm
@@ -0,0 +1,35 @@
+global _start
+%include "lib.inc"
+
+%define pc r15
+%define w r14
+%define rstack r13
+
+section .text
+
+%include "native_words.inc"
+
+section .bss
+resq 1023
+rstack_start: resq 1
+user_mem: resq 65536
+
+section .data
+
+last_word: dq _lw
+dp: dq user_mem
+stack_start:  dq 0
+ 
+section .text
+
+_start: 
+    mov rstack, rstack_start
+    mov [stack_start], rsp
+    mov pc, forth_init
+
+next:                  
+    mov w, pc
+    add pc, 8
+    mov w, [w]
+    jmp [w]
+
