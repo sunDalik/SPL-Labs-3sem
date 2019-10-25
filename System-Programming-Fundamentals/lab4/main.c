efef2f4ee7690fa7812b88dabcfb26d92e959e49
sunDalik <dali1999@mail.ru>
2019-10-25 20:32:23 +0300

    Lab4 (tee) on C

---
+++ System-Programming-Fundamentals/lab4/main.c
@@ -0,0 +1,81 @@
+#include <errno.h>
+#include <getopt.h>
+#include <string.h>
+#include <malloc.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+extern int errno;
+
+const char *UNKNOWN_OPTION_MESSAGE = "Invalid option\n";
+const char *USAGE_MESSAGE = "Usage: tee [-ai] [FILE]...\n";
+
+int append_mode = 0;
+int ignore_interrupts_mode = 0;
+int errors = 0;
+
+void handler_interruption() {
+    if (!ignore_interrupts_mode) {
+        write(2, "\n", 1);
+        exit(errors);
+    }
+}
+
+/* tee */
+int main(int argc, char *argv[]) {
+    signal(SIGINT, handler_interruption);
+    int rez = 0;
+    while ((rez = getopt(argc, argv, "ai")) != -1) {
+        switch (rez) {
+            case 'a':
+                append_mode = 1;
+                break;
+            case 'i':
+                ignore_interrupts_mode = 1;
+                break;
+            case '?':
+                write(2, UNKNOWN_OPTION_MESSAGE, strlen(UNKNOWN_OPTION_MESSAGE));
+                write(2, USAGE_MESSAGE, strlen(USAGE_MESSAGE));
+                return 1;
+        }
+    }
+
+    const int FILE_CNT = argc - optind;
+    FILE **files = malloc(FILE_CNT * sizeof(int));
+    int f_i = 0;
+    for (int i = optind; i < argc; ++i) {
+        if (append_mode) {
+            files[f_i] = fopen(argv[i], "a");
+        } else {
+            files[f_i] = fopen(argv[i], "w");
+        }
+
+        if (files[f_i] == NULL) {
+            errors = 1;
+            if (errno == EACCES) {
+                fprintf(stderr, "Permission denied: %s\n", argv[i]);
+            }
+        }
+
+        f_i++;
+    }
+
+
+    char input;
+    while (read(0, &input, 1) > 0) {
+        write(1, &input, 1);
+        for (int i = 0; i < FILE_CNT; ++i) {
+            if (files[i] != NULL) {
+                fputc(input, files[i]);
+            }
+        }
+    }
+
+    for (int i = 1; i < FILE_CNT; ++i) {
+        if (files[i] != NULL) fclose(files[i]);
+    }
+    free(files);
+    return errors;
+}
