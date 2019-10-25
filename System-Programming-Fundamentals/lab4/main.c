20cc37d17cc9eb04170160a636ad305a04bbc880
sunDalik <dali1999@mail.ru>
2019-10-25 20:52:24 +0300

    Change variable names

--- System-Programming-Fundamentals/lab4/main.c
+++ System-Programming-Fundamentals/lab4/main.c
@@ -9,14 +9,14 @@
 
 extern int errno;
 
-const char *UNKNOWN_OPTION_MESSAGE = "Invalid option\n";
+const char *INVALID_OPTION_MESSAGE = "Invalid option\n";
 const char *USAGE_MESSAGE = "Usage: tee [-ai] [FILE]...\n";
 
 int append_mode = 0;
 int ignore_interrupts_mode = 0;
 int errors = 0;
 
-void handler_interruption() {
+void handle_interruptions() {
     if (!ignore_interrupts_mode) {
         write(2, "\n", 1);
         exit(errors);
@@ -25,7 +25,7 @@ void handler_interruption() {
 
 /* tee */
 int main(int argc, char *argv[]) {
-    signal(SIGINT, handler_interruption);
+    signal(SIGINT, handle_interruptions);
     int rez = 0;
     while ((rez = getopt(argc, argv, "ai")) != -1) {
         switch (rez) {
@@ -36,14 +36,14 @@ int main(int argc, char *argv[]) {
                 ignore_interrupts_mode = 1;
                 break;
             case '?':
-                write(2, UNKNOWN_OPTION_MESSAGE, strlen(UNKNOWN_OPTION_MESSAGE));
+                write(2, INVALID_OPTION_MESSAGE, strlen(INVALID_OPTION_MESSAGE));
                 write(2, USAGE_MESSAGE, strlen(USAGE_MESSAGE));
                 return 1;
         }
     }
 
-    const int FILE_CNT = argc - optind;
-    FILE **files = malloc(FILE_CNT * sizeof(int));
+    const int FILE_COUNT = argc - optind;
+    FILE **files = malloc(FILE_COUNT * sizeof(int));
     int f_i = 0;
     for (int i = optind; i < argc; ++i) {
         if (append_mode) {
@@ -66,14 +66,14 @@ int main(int argc, char *argv[]) {
     char input;
     while (read(0, &input, 1) > 0) {
         write(1, &input, 1);
-        for (int i = 0; i < FILE_CNT; ++i) {
+        for (int i = 0; i < FILE_COUNT; ++i) {
             if (files[i] != NULL) {
                 fputc(input, files[i]);
             }
         }
     }
 
-    for (int i = 1; i < FILE_CNT; ++i) {
+    for (int i = 1; i < FILE_COUNT; ++i) {
         if (files[i] != NULL) fclose(files[i]);
     }
     free(files);
