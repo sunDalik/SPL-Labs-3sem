fd30a9943d9cc78d2e8dfe6c95ffefbf08466ab9
sunDalik <dali1999@mail.ru>
2019-10-25 22:13:59 +0300

    Fix makefile

--- System-Programming-Fundamentals/lab4/lab4.pl
+++ System-Programming-Fundamentals/lab4/lab4.pl
@@ -1,5 +1,22 @@
 #!/usr/bin/perl -T
 use strict;
 use warnings qw(FATAL all);
+use Getopt::Long;
+
+my $ignore_interrupts_mode = 0;
+my $append_mode = 0;
+my $errors = 0;
+
+$SIG{'INT'} = \&handle_interruptions;
+
+sub handle_interruptions {
+	if(!$ignore_interrupts_mode){
+		print "\n";
+		exit $errors;
+	}
+}
+
+GetOptions("a" => \$append_mode,
+	"i" => \$ignore_interrupts_mode)
 
 
