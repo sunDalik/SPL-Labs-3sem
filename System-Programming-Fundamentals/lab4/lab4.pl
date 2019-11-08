b9074e75455d656fa7850751b68fd36ce89def4f
sunDalik <dali1999@mail.ru>
2019-11-08 19:12:02 +0300

    Perl script: reading input and writing to file

--- System-Programming-Fundamentals/lab4/lab4.pl
+++ System-Programming-Fundamentals/lab4/lab4.pl
@@ -6,6 +6,10 @@ use Getopt::Long;
 my $ignore_interrupts_mode = 0;
 my $append_mode = 0;
 my $errors = 0;
+my $i = 0;
+my @files;
+my $file;
+my $line;
 
 $SIG{'INT'} = \&handle_interruptions;
 
@@ -17,6 +21,24 @@ sub handle_interruptions {
 }
 
 GetOptions("a" => \$append_mode,
-	"i" => \$ignore_interrupts_mode)
+	"i" => \$ignore_interrupts_mode);
 
+foreach $file(@ARGV) {
+	if($append_mode == 1){
+		open($files[$i], ">>", $file) or die "Can't open file $file: $!";
+	} else {
+		open($files[$i], ">", $file) or die "Can't open file $file: $!"; 
+	}
+	$i++;
+}
 
+foreach $line(<STDIN>) {
+	foreach $file(@files) {
+		print $file $line;
+	}
+	print $line;
+}
+
+foreach $file(@files) {
+	close($file);
+}
