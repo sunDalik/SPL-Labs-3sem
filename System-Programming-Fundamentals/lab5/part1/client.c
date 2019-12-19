68b40d435f2a52d76e97d700d9170ade9c096dac
sunDalik <dali1999@mail.ru>
2019-12-19 17:22:59 +0300

    Init lab5

---
+++ System-Programming-Fundamentals/lab5/part1/client.c
@@ -0,0 +1,80 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <stdbool.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include "system_info.h"
+
+bool parse_int(const char *str, int *var) {
+    char *end;
+    *var = (int) strtol(str, &end, 10);
+    if (end == str) return false;
+    return true;
+}
+
+int main(int argc, char *argv[]) {
+    struct system_info *sys_info = NULL;
+
+    // setup client from args
+    int opt = 0;
+    int connected = false;
+    while ((opt = getopt(argc, argv, "v:m:q:")) != -1) {
+        switch (opt) {
+            case 'v':
+                printf("system-v mode\n");
+                // getting system_info structure from system V memory segment
+                int sysVMemID;
+                parse_int(optarg, &sysVMemID);
+                printf("id = %u\n\n", sysVMemID);
+                sys_info = (struct system_info *) shmat(sysVMemID, NULL, 0);
+                connected = true;
+                break;
+            case 'm':
+                printf("mmap mode\n");
+                printf("name = %s\n\n", optarg);
+                // opening file and getting system_info structure from it
+                int mmapFD = open(optarg, O_RDWR, 0644);  // open file
+                sys_info = (struct system_info *) mmap(NULL, sizeof(struct system_info),
+                                                       PROT_READ, MAP_SHARED, mmapFD, 0); // map file to memory
+                connected = true;
+                break;
+            case 'q':
+                printf("message-queue mode\n");
+                // initialize message queue
+                int msgQID;
+                parse_int(optarg, &msgQID);
+                printf("id = %u\n\n", msgQID);
+                // send message
+                msgbuf_t msg;
+                msg.mtype = MSGTYPE_QUERY;
+                msgsnd(msgQID, &msg, 0, 0);
+                // receive message
+                msgrcv(msgQID, &msg, sizeof(struct system_info), MSGTYPE_REPLY, 0);
+                sys_info = (struct system_info *) malloc(sizeof(struct system_info));
+                memcpy(sys_info, msg.mtext, sizeof(struct system_info));
+                connected = true;
+                break;
+        }
+    }
+
+    if (connected == false) {
+        fprintf(stderr, "Usage: %s [-v] [-m] [-q]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("data received\n");
+    printf("time = %lu\n", sys_info->startup_time);
+    printf("pid = %u\n", sys_info->pid);
+    printf("uid = %u\n", sys_info->uid);
+    printf("gid = %u\n", sys_info->gid);
+    printf("average system load in 1min = %f\n", sys_info->sys_loads[0]);
+    printf("average system load in 2min = %f\n", sys_info->sys_loads[1]);
+    printf("average system load in 5min = %f\n", sys_info->sys_loads[2]);
+}
---
+++ System-Programming-Fundamentals/lab5/part1/server.c
@@ -0,0 +1,145 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <time.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <sys/mman.h>
+#include <signal.h>
+#include <fcntl.h>
+#include "system_info.h"
+
+#define UNDEFMODE 0
+#define SYSVMODE 1
+#define MMAPMODE 2
+#define MSGQMODE 3
+
+int mmapFD;     // mmap file descriptor
+char filename[12];  // all numbers are representable by str[12]
+struct system_info *sys_info_ptr;
+void *sysVMemPointer;
+int sysVMemID; // system V memory segment id
+
+void shutdown_server() {
+    printf("\nServer terminated\n");
+    if (sysVMemPointer) {
+        shmdt(sysVMemPointer);
+        printf("System-v-pointer released\n");
+    } else if (mmapFD) {
+        remove(filename);
+        printf("mmap file deleted\n");
+    }
+    exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    // initialize system info struct
+    // storing pid, uid, gid
+    pid_t pid = getpid();
+    uid_t uid = getuid();
+    gid_t gid = getgid();
+    // initialize start time
+    time_t start_time = time(NULL);
+    signal(SIGINT, shutdown_server);
+
+    int msgQID; // message queue id
+    msgbuf_t msg; // message
+
+    // server setup from args
+    int opt = 0, run_mode = UNDEFMODE;
+    while ((opt = getopt(argc, argv, "vmq")) != -1) {
+        switch (opt) {
+            case 'v':
+                run_mode = SYSVMODE;
+                // initialize system v memory segment and set system info pointer to
+                // system v pointer
+                sysVMemID = shmget(IPC_PRIVATE, sizeof(struct system_info), IPC_CREAT | 0644);
+                sysVMemPointer = shmat(sysVMemID, NULL, 0);
+                sys_info_ptr = (struct system_info *) sysVMemPointer;
+                break;
+            case 'm':
+                run_mode = MMAPMODE;
+                // create file and map it to memory
+                sprintf(filename, "%d", pid);
+                mmapFD = open(filename, O_RDWR | O_CREAT, 0644);  // create file
+                ftruncate(mmapFD, sizeof(struct system_info));    // set file length
+                sys_info_ptr = (struct system_info *) mmap(NULL, sizeof(struct system_info),
+                                                           PROT_WRITE | PROT_READ, MAP_SHARED, mmapFD,
+                                                           0); // map file to memory
+                break;
+            case 'q':
+                run_mode = MSGQMODE;
+                // initialize system_info structure
+                sys_info_ptr = (struct system_info *) malloc(sizeof(struct system_info));
+                // initialize message queue
+                msgQID = msgget(IPC_PRIVATE, IPC_CREAT | 0644);
+                break;
+        }
+    }
+    if (run_mode == UNDEFMODE) {
+        fprintf(stderr, "Usage: %s [-v] [-m] [-q]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Init parameters:\n");
+    printf("pid = %u\n", pid);
+    printf("uid = %u\n", uid);
+    printf("gid = %u\n\n", gid);
+
+    switch (run_mode) {
+        case SYSVMODE:
+            printf("system-v-shared-memory mode\n");
+            printf("Created system-v-memory-segment\n");
+            printf("id = %u\n", sysVMemID);
+            printf("size = %lu bytes\n\n", sizeof(struct system_info));
+            break;
+        case MMAPMODE:
+            printf("mmap mode\n");
+            printf("Created new file:\n");
+            printf("name = %s\n", filename);
+            printf("size = %lu bytes\n\n", sizeof(struct system_info));
+            break;
+        case MSGQMODE:
+            printf("system-v-message-queue mode\n");
+            printf("Created new queue\n");
+            printf("id = %u\n\n", msgQID);
+            break;
+    }
+
+
+    // initialize sys_info struct
+    sys_info_ptr->pid = pid;
+    sys_info_ptr->uid = uid;
+    sys_info_ptr->gid = gid;
+    sys_info_ptr->startup_time = time(NULL) - start_time;
+    getloadavg(sys_info_ptr->sys_loads, 3);
+
+    printf("Server is running\n");
+
+    // server mainloop
+    while (1) {
+        if (run_mode == MSGQMODE) {
+            // receive message
+            msgrcv(msgQID, &msg, 0, MSGTYPE_QUERY, 0);
+            // updating current time
+            sys_info_ptr->startup_time = time(NULL) - start_time;
+            // updating average system load
+            getloadavg(sys_info_ptr->sys_loads, 3);
+            // send reply
+            msg.mtype = MSGTYPE_REPLY;
+            memcpy(msg.mtext, sys_info_ptr, sizeof(struct system_info));
+            msgsnd(msgQID, &msg, sizeof(struct system_info), 0);
+        } else {
+            // sleeping for 1 second
+            sleep(1);
+            // updating current time
+            sys_info_ptr->startup_time = time(NULL) - start_time;
+            // updating average system load
+            getloadavg(sys_info_ptr->sys_loads, 3);
+        }
+    }
+}
