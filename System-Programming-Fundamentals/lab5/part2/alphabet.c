b4adcae5aa71a91dc60994a54b001a753c9931b5
sunDalik <dali1999@mail.ru>
2019-12-20 18:06:09 +0300

    Refactor lab5 alphabet.c

--- System-Programming-Fundamentals/lab5/part2/alphabet.c
+++ System-Programming-Fundamentals/lab5/part2/alphabet.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
 #include <ctype.h>
 
-char alphabet[26] = {
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-};
+char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 void print_alphabet() {
     printf("\n");
-    char *alpha = alphabet;
-    while (*alpha) {
-        printf("%c ", *alpha);
-        alpha++;
-    }
+    for (int i = 0; i < 26; ++i)
+        printf("%c ", alphabet[i]);
     printf("\n");
 }
 
 void invert_case() {
     for (int i = 0; i < 26; ++i)
-        alphabet[i] = (islower(alphabet[i])) ? toupper(alphabet[i]) : tolower(alphabet[i]);
+        alphabet[i] = islower(alphabet[i]) ? toupper(alphabet[i]) : tolower(alphabet[i]);
 }
 
 void swap_alphabet() {
     int i = 0, j = 25;
-    char c;
     while (i < j) {
-        c = alphabet[i];
+        char temp = alphabet[i];
         alphabet[i] = alphabet[j];
-        alphabet[j] = c;
+        alphabet[j] = temp;
         i++;
         j--;
     }
